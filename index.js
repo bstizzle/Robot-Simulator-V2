d5652c5b288448a899fc782d688a39ee483b2d9d
bstizzle <whereyouben@gmail.com>
2020-12-15 14:29:03 -0500

    got arrowkey input

--- index.js
+++ index.js
@@ -3,4 +3,32 @@ document.addEventListener("DOMContentLoaded", function() {
   init();
 
   // ADD CODE HERE!
+  const ul = document.querySelector("#moves-container");
+
+  function addMove(event){
+    if(event.key === "ArrowDown"){
+      let down = document.createElement("li");
+      down.innerText = "down";
+      ul.appendChild(down);
+    } else if(event.key === "ArrowUp") {
+      let up = document.createElement("li");
+      up.innerText = "up";
+      ul.appendChild(up);
+    } else if(event.key === "ArrowLeft") {
+      let left = document.createElement("li");
+      left.innerText = "left";
+      ul.appendChild(left);
+    } else if(event.key === "ArrowRight"){
+      let right = document.createElement("li");
+      right.innerText = "right";
+      ul.appendChild(right);
+    };
+  };
+
+  document.addEventListener("keydown", addMove);
+
+  const moveButton = document.querySelector("#move-button");
+
+
+  
 });
